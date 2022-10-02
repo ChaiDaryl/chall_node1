4bae80de247bb468eafffda65bc77c3353ca0e6d
ChaiDaryl <chaigne.d01@gmail.com>
2022-10-02 16:10:25 +0200

    quete node1

---
+++ index.js
@@ -0,0 +1,10 @@
+const userInfo = require('./information.js');
+
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hello Daryl from lyon ",
+    e : "^^",
+    T : "U",
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+let userInfo = {
+    userName: "daryl",
+    location: "lyon",
+}
+
+module.exports = {
+        userInfo,  
+   
+};
